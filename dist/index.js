a06df235dd9a7d419a84b43b63dd3b1d665a2cf3
Prem <pprakash312003@gmail.com>
2024-09-03 18:45:54 +0530

    last

--- dist/index.js
+++ dist/index.js
@@ -1,61 +1,28 @@
-"use strict";
-// // import { PrismaClient } from "@prisma/client";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// // const prisma = new PrismaClient();
-// // async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-// //   const res = await prisma.user.create({
-// //     data: {
-// //         username,
-// //         password,
-// //         firstName,
-// //         lastName
-// //     }
-// //   })
-// //   console.log(res);
-// // }
-// // insertUser("admin1", "123456", "harkirat", "singh")
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
-// interface UpdateParams {
-//     firstName: string;
-//     lastName: string;
+// "use strict";
+// var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+//     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+//     return new (P || (P = Promise))(function (resolve, reject) {
+//         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+//         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+//         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+//         step((generator = generator.apply(thisArg, _arguments || [])).next());
+//     });
+// };
+// Object.defineProperty(exports, "__esModule", { value: true });
+// const client_1 = require("@prisma/client");
+// const prisma = new client_1.PrismaClient();
+// function insertUser(username, password, firstName, lastName) {
+//     return __awaiter(this, void 0, void 0, function* () {
+//         const res = yield prisma.user.create({
+//             data: {
+//                 username,
+//                 password,
+//                 firstName,
+//                 lastName
+//             }
+//         });
+//         console.log(res);
+//     });
 // }
-// async function updateUser(username: string, {
-//   firstName,
-//   lastName
-// }: UpdateParams) {
-// const res = await prisma.user.update({
-//   where: { username },
-//   data: {
-//     firstName,
-//     lastName
-//   }
-// });
-// console.log(res);
-// }
-// updateUser("admin2", {
-//   firstName: "new name",
-//   lastName: "rajput"
-// }) 
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-function getUser(username) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const user = yield prisma.user.findFirst({
-            where: {
-                username: username
-            }
-        });
-        console.log(user);
-    });
-}
-getUser("admin1");
+// insertUser("admin2", "123457", "prem", "prakash");
+
